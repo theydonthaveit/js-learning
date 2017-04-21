3b9a4cfe71af9161538b16618e15d05899578649
Alan Williams <theydonthaveit@gmail.com>
2017-04-21 16:30:22 +0000

    added vuex and store for management, stocks works from state management now

---
+++ trader/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    {id: 1, name: 'BMW', price: 110},
+    {id: 1, name: 'Google', price: 640},
+    {id: 1, name: 'Apple', price: 320},
+    {id: 1, name: 'Twitter', price: 10}
+]
